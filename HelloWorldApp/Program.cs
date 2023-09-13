e720d32184b86f4eebb024f75a19cb22cbeaad1b
p-tomczak <ptomczak98@gmail.com>
2023-09-13 15:19:09 +0200

    MOD Tekst na konsoli

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World! :)");
             Console.ReadKey();
         }
     }
